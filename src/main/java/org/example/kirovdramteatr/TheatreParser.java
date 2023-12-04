854298c9b0619b3577fd222ad439cb908a795465
Denikaso <daniil.tata@mail.ru>
2023-12-04 19:24:49 +0300

    Рефакторинг класса парсера сайта https://kirovdramteatr.ru/afisha

--- src/main/java/org/example/kirovdramteatr/TheatreParser.java
+++ src/main/java/org/example/kirovdramteatr/TheatreParser.java
@@ -1,19 +1,29 @@
 package org.example.kirovdramteatr;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.exceptions.ParsingRuntimeException;
 import org.example.models.Poster;
-import lombok.val;
 import org.example.parser.Parser;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import java.util.Optional;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import lombok.val;
 
 public class TheatreParser implements Parser<ArrayList<Poster>> {
+    private static final Logger logger = LogManager.getLogger(TheatreParser.class);
+    private static final String NO_DATA = "Нет данных";
+
     @Override
     public final ArrayList<Poster> parse(Document document) {
         ArrayList<Poster> posters = new ArrayList<>();
@@ -22,61 +32,80 @@ public class TheatreParser implements Parser<ArrayList<Poster>> {
 
         try {
             Files.createDirectories(folderPath);
-        } catch(Exception e) {
-            e.printStackTrace();
+            logger.info("Созданы директории для сохранения изображений.");
+        } catch (IOException e) {
+            logger.error("Ошибка при создании директорий", e);
+            throw new ParsingRuntimeException("Ошибка при создании директорий", e);
         }
 
-        for(Element poster : postersElements){
-            val duration = poster
-                    .select(".td3 .td2 .td1 div")
-                    .first()
-                    .textNodes()
-                    .get(0)
-                    .text();
-            val td2Element = poster
-                    .select(".td2")
-                    .first();
-            val imageUrl = td2Element
-                    .select("img")
-                    .first()
-                    .absUrl("src");
-            val tInfoAfishaElement = poster
-                    .select(".t_info_afisha")
-                    .first();
-            val date = tInfoAfishaElement
-                    .select(".td1 .date_afisha").text();
-            val title = tInfoAfishaElement.select("h3 a")
-                    .textNodes()
-                    .get(0)
-                    .text();
-            val ageLimit = tInfoAfishaElement.select(".value_limit")
-                    .text();
-            posters.add(Poster.builder()
-                    .title(title)
-                    .imageUrl(imageUrl)
-                    .date(date)
-                    .duration(duration)
-                    .ageLimit(ageLimit)
-                    .build());
+        for (Element poster : postersElements) {
+            try {
+                val duration = Optional.ofNullable(poster
+                                .select(".td3 .td2 .td1 div")
+                                .first())
+                        .map(element -> element.textNodes().get(0).text())
+                        .orElse(NO_DATA);
 
-            if(imageUrl.startsWith("https")) {
-                try {
-                    val url = new URL(imageUrl);
+                val td2Element = poster.select(".td2").first();
+                val imageUrl = Optional.ofNullable(td2Element)
+                        .map(element -> element.select("img").first())
+                        .map(element -> element.absUrl("src"))
+                        .orElse(NO_DATA);
 
-                    try (val in = url.openStream()) {
-                        val fileName = imageUrl
-                                .substring(imageUrl.lastIndexOf('/') + 1);
-                        val imagePath = Paths
-                                .get(folderPath.toString(),  imageUrl.substring(imageUrl.lastIndexOf('/') + 1));
-                        Files.copy(in, imagePath, StandardCopyOption.REPLACE_EXISTING);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
+                val tInfoAfishaElement = poster.select(".t_info_afisha").first();
+                val date = Optional.ofNullable(tInfoAfishaElement)
+                        .map(element -> element.select(".td1 .date_afisha").text())
+                        .orElse(NO_DATA);
+
+                val title = Optional.ofNullable(tInfoAfishaElement)
+                        .map(element -> element.select("h3 a").textNodes().get(0).text())
+                        .orElse(NO_DATA);
+
+                val ageLimit = Optional.ofNullable(tInfoAfishaElement)
+                        .map(element -> element.select(".value_limit").text())
+                        .orElse(NO_DATA);
+
+                posters.add(Poster.builder()
+                        .title(title)
+                        .imageUrl(imageUrl)
+                        .date(date)
+                        .duration(duration)
+                        .ageLimit(ageLimit)
+                        .build());
+
+                if (imageUrl.startsWith("https")) {
+                    copyImage(imageUrl, folderPath);
                 }
+
+            } catch (Exception e) {
+                logger.error("Ошибка при обработке данных афиши", e);
+                throw new ParsingRuntimeException("Ошибка при обработке данных афиши", e);
             }
         }
         return posters;
+    }
+
+    private void copyImage(String imageUrl, Path folderPath) {
+        try {
+            URI uri = new URI(imageUrl);
+            URL url = uri.toURL();
+
+            String fileName = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+            Path imagePath = Paths.get(folderPath.toString(), fileName);
+            downloadImage(url, imagePath);
+            logger.info("Изображение скопировано успешно: {}", fileName);
+        } catch (URISyntaxException | MalformedURLException e) {
+            logger.error("Ошибка при обработке URL", e);
+            throw new ParsingRuntimeException("Ошибка при обработке URL", e);
+        }
+    }
+
+    private void downloadImage(URL url, Path imagePath) {
+        try (val in = url.openStream()) {
+            Files.copy(in, imagePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            logger.error("Ошибка при загрузке изображения", e);
+            throw new ParsingRuntimeException("Ошибка при загрузке изображения", e);
+        }
     }
 }
